2ecbb8206119764bd20f904eed70cf0b072ded57
uniiun <pg3252@proton.me>
2024-06-28 15:36:30 +0200

    added configuration for rust, vue and tailwind

--- src/organiczer_frontend/src/App.vue
+++ src/organiczer_frontend/src/App.vue
@@ -18,6 +18,7 @@ async function handleSubmit(e) {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    <h1 class="text-blue-600 text-center">Tailwind działa!!!</h1>
     <form action="#" @submit="handleSubmit">
       <label for="name">Enter your name: &nbsp;</label>
       <input id="name" alt="Name" type="text" />
